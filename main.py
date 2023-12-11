0a888d05869650b94fbfbc3ba624eb69f7a1d739
egordgan302 <143101848+egordgan302@users.noreply.github.com>
2023-12-11 09:16:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+class RoboCat:
+    def __init__(self, speed, sound, color):
+        self.ears = 2
+        self.hp = 9
+        self.speed = speed
+        self.sound = sound
+        self.color = color
 
+    def play_sounde(self):
+        print(f'My robocat say {self.sound}')
+
+class angryrobocat(RoboCat):
+    def __init__(self, speed, hp, color):
+        super().__init__(speed, hp, color)
+        self.superpower = 'стрелять лазерами'
+    def damage(self, damage):
+        self.hp -= damage
+        print(f'осталось {self.hp}')
+
+my_robocat = RoboCat(10, 'meow', 'black')
+print (my_robocat.color)
+
+my_robocat.play_sounde()
+
+angry = angryrobocat(100, 9, 'red')
+angry.damage(2)
